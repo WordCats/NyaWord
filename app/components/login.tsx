20fe383ea740b87a9eb30abf19a32c137213a209
kami-note <104632447+kami-note@users.noreply.github.com>
2022-11-27 18:39:52 -0300

    Atualização de tela de login!
    Reconfigurei os arquivos do tailwind e refiz a pagina de login.

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -1,83 +1,29 @@
 export default function Login() {
   return (
-    <div>
-      <div className="flex min-h-full items-center justify-center py-6 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-6">
-          <div>
-            <img
-              className="mx-auto h-48 w-auto"
-              src="https://i.imgur.com/YM2x83p.png"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-white">
-              Entre no mundo do Nya Word 🐱‍💻
-            </h2>
-          </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Email address"
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
-            
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-white focus:ring-indigo-500"
-                />
-                <label htmlFor="remember-me" className="ml-2 block text-sm text-white">
-                  Remember me
-                </label>
-              </div>
-
-              <div className="text-sm">
-                <a href="#" className="font-medium text-white hover:text-white">
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-rose py-2 px-4 text-sm font-medium text-white hover:bg-rosee focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                </span>
-                Sign in
-              </button>
-            </div>
-          </form>
+    <div className="w-scree h-screen flex items-center justify-center">
+      <div className="bg-sky h-screen flex justify-center items-center w-screen">
+        <div className="pl-3">
+          <h1 className="text-6xl text-gray-100 font-semibold ">Nya Word! 🐱‍🚀</h1><br></br>
+          <h2 className="text-4xl text-gray-300">O Lugar onde você compartilha seu <a className="text-5xl underline decoration-pink-500 cursor-pointer">mundo de historia!</a><br />Leia as incriveis historias que todos tem a contar.</h2>
         </div>
+      </div>
+      <div className="h-screen flex justify-center items-center pl-56 pr-56">
+        <div className="w-96 h-96 shadow-2xl rounded-lg">
+          <div className="m-12 space-y-4">
+            <h1 className="text-3xl text-center">Entre agora!</h1>
+            <input placeholder="E-mail" className="border-2 rounded-md w-72 h-10 pl-3" />
+            <input placeholder="Senha" className="border-2 rounded-md w-72 h-10 pl-3" />
+            <a className="cursor-pointer pt-2 text-cyan-600">Esqueceu a senha?</a>
+            <div className="text-center space-y-4">
+              <button className="cursor-pointer rounded w-40 h-8 bg-rose-500 hover:bg-rose-400 text-gray-100 ">Entrar</button>
+              <hr />
+              <button className="cursor-pointer rounded w-40 h-8 bg-cyan-700 hover:bg-cyan-500 text-gray-100 ">Criar conta!</button>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
-  </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,72 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-
-      black: '#242323',
-      white: '#FFF',
-
-      background: '#18181B',
-      bluesky: '#53D8FB',
-      blueocean: '#66C3FF',
-      whitecoffe: '#DCE1E9',
-      pastelpink: '#D4AFB9',
-
-      rose: '#F43F5E',
-      rosee: '#FF7291',
-
-
-      default: {
-        DEFAULT: '#596172',
-        600: '#373b45',
-      },
-
-      primary: {
-        DEFAULT: '#17BEBB',
-        600: '#108583',
-      },
-
-      secondary: {
-        DEFAULT: '#25272E',
-        600: '#32343d',
-      },
-      tertiary: '#E9DCC4',
-
-      error: {
-        DEFAULT: '#A71D31',
-        600: '#7a1726',
-      },
-
-      info: '#4969B3',
-    },
-    fontFamily: {
-      title: ['Josefin Sans', 'sans-serif'],
-      text: ['Nunito Sans', 'sans-serif'],
-    },
     extend: {
-      spacing: {
-        sm: '8px',
-        md: '12px',
-        lg: '16px',
-        xl: '24px',
-        vh: '8px 24px'
-      },
-      borderRadius: {
-        'none': '0',
-        'sm': '4px',
-        'md': '8px',
-        'lg': '12px',
-        'xl': '16px',
-        'full': '9999px'
-      },
-      /*
-      fontFamily: {
-        sans: 'Inter, sans-serif'
-      },
-      */
-    },
+      backgroundImage: {
+        'sky': "url('app/styles/loginBackground.jpg')",
+      }
+    }
   },
-  plugins: [],
-}
+  variants: {},
+  plugins: []
+};
