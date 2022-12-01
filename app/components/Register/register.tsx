e767e550080db369cf86b6bd9065c3681ec3ae97
luizgmelo <88911920+luizgmelo@users.noreply.github.com>
2022-12-01 09:33:40 -0300

    criei o estilo do componente register

--- app/components/Register/register.tsx
+++ app/components/Register/register.tsx
@@ -1,40 +1,38 @@
 export default function Register() {
     return (
-        <div>
-            <div>
-                <h1 className="bg-red-700 h-24">CABEÇALHO</h1>
+        <div className="bg-sky h-screen">
+            <div className="flex items-center h-20 mb-3">
+                <h1 className="m-5 text-3xl text-white">NyaWorld</h1>
             </div>            
 
-            <div className="flex flex-col items-center bg-blue-700 p-3 w-2/5">
-                <h1>Criar uma nova conta</h1>
-                <form>
-                    <div className="flex gap-x-10 m-3">
-                        <div className="flex gap-x-2">
-                            <label htmlFor="inome">Nome:</label>
-                            <input type="text" name="nome" id="inome" autoComplete="given-name" required/>
-                        </div>
-
-                        <div className="flex gap-x-2">
-                            <label htmlFor="isobrenome">Sobrenome:</label>
-                            <input type="text" name="sobrenome" id="isobrenome" autoComplete="family-name" required />
-                        </div>
-                    </div>
-
-                    <div className="flex gap-x-2 m-3">
-                        <label htmlFor="iemail">E-mail:</label>
-                        <input type="email" name="email" id="iemail" autoComplete="off" required />
-                    </div>
-
-                    <div className="flex gap-x-2 m-3">
-                        <label htmlFor="isenha">Senha:</label>
-                        <input type="password" name="senha" id="isenha" minLength={8} required />
-                    </div>
-                    
-                    <div className="flex justify-center">
-                        <input className="bg-red-700 p-1 my-3" type="submit" value="Cadastre-se" />
-                    </div>
-                </form>
-                <a className="text-blue-300 p-1 my-3" href="../Login">Já tem uma conta?</a>
+            <div className="flex flex-col items-center justify-center w-2/5 h-5/6 ">
+                <div className="bg-white rounded-3xl p-5">
+                   <h1 className="text-4xl text-center">Criar uma nova conta</h1>
+                       <form>
+                            <div className="flex gap-x-10 m-3 my-10">
+                                <div className="flex gap-x-2 items-center">
+                                    <label htmlFor="inome">Nome:</label>
+                                    <input className="border-solid border-2 border-black-100 rounded h-10" type="text" name="nome" id="inome" autoComplete="given-name" required/>
+                                </div>
+                                <div className="flex gap-x-2 items-center">
+                                    <label htmlFor="isobrenome">Sobrenome:</label>
+                                    <input className="border-solid border-2 border-black-100 rounded h-10" type="text" name="sobrenome" id="isobrenome" autoComplete="family-name" required />
+                                </div>
+                            </div>
+                            <div className="flex gap-x-2 m-3 my-10 items-center">
+                                <label htmlFor="iemail">E-mail:</label>
+                                <input className="border-solid border-2 border-black-100 rounded h-10" type="email" name="email" id="iemail" autoComplete="off" required />
+                            </div>
+                            <div className="flex gap-x-2 m-3 my-10 items-center">
+                                <label htmlFor="isenha">Senha:</label>
+                                <input className="border-solid border-2 border-black-100 rounded h-10" type="password" name="senha" id="isenha" minLength={8} required />
+                            </div>
+                            <div className="flex justify-center">
+                                <input className="text-white bg-black p-3 rounded cursor-pointer hover:bg-gray-300 hover:text-black" type="submit" value="Cadastre-se" />
+                            </div>
+                        </form>
+                        <a className="flex text-blue-700 p-1 my-3 justify-center hover:text-gray-900" href="../Login">Já tem uma conta?</a>
+                </div>
             </div>
         </div>
     )
